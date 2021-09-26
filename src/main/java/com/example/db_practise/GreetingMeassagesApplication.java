94d05e54a427848357881e6c0f11fc0c8ce24dff
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-26 14:17:23 +0530

    [Nikhil] - added intial setup

---
+++ src/main/java/com/example/db_practise/GreetingMeassagesApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_practise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreetingMeassagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingMeassagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/db_practise/GreetingMeassagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db_practise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingMeassagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
