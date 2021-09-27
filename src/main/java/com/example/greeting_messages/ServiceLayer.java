3549c28b7a39d650b1e3c543245f326d5b8852b7
Nikhil Musale <nikhilmusale70@gmail.com>
2021-09-27 18:51:34 +0530

    [Nikhil]- added a service layer giving hello world

---
+++ src/main/java/com/example/greeting_messages/ServiceLayer.java
@@ -0,0 +1,12 @@
+package com.example.greeting_messages;
+
+import org.springframework.stereotype.Service;
+
+
+public class ServiceLayer {
+	
+	public String hi(){
+		return "Hello World";
+	}
+
+}
--- src/main/java/com/example/greeting_messages/greetingsContainer.java
+++ src/main/java/com/example/greeting_messages/greetingsContainer.java
@@ -10,6 +10,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.greeting_messages.container.greetMessages;
@@ -20,11 +21,23 @@ public class greetingsContainer {
 	
 	greetMessages greet;
 	
-	
 	@GetMapping(value = "/greet", produces = "application/json")
 	public greetMessages hello() {
 		greet = new greetMessages("Hi GoodAfterNoon");
 		return greet ;
 	}
+	
+	@GetMapping(value = "/greeting", produces = "application/json")
+	public String hii() {
+		ServiceLayer sLayer = new ServiceLayer();
+		return sLayer.hi();
+	}
+	
+	@GetMapping(value = "/greetWithName")
+	public String greetName(@RequestParam String name) {
+		greet = new greetMessages("Hi GoodAfterNoon");
+		return name + " " + greet.getS1() ;
+	}
+	
 
 }
